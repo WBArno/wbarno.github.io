b4aead468b7a8ffe7843aecb5b81e90cc3f99eff
Will <mail@williamarnold.info>
2025-01-07 17:27:55 -0500

    Add initial VSCode DevContainer files for use with local docker container.

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,12 @@
+# Based on DotNetPortfolio Template
+# sudo apt-get update \
+#     && sudo apt upgrade -y \
+#     && sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils \
+#     && sudo apt clean -y \
+#     && sudo rm -rf /var/lib/apt/lists/*
+
+# Start Hugo Server
+hugo mod tidy
+hugo mod npm pack
+npm install
+hugo server -w
